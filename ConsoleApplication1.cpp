29331fbd8c817098bd980364f755f00fb57420e2
midtvu2 <harhehahah@gmail.com>
2025-10-01 10:32:19 -0500

    upload program
    mnbnnbnebmsbfnsemfg

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int y = 0;
+   // creates the X and Y variable
+    cout << "Enter the X variable now!" << endl;
+    cin >> x;
+    cout << endl;
+   // prompts user for the X variable
+    cout << "Enter the Y variable now!" << endl;
+    cin >> y;
+    cout << endl;
+    //prompts user for the Y variable
+    cout << "Your answer is " << x + y << "!" << endl;
+    // adds the variables together
+    return 0;
+
+
+
+    ;
+}
+
